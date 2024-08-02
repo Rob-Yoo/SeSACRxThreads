36ed60de06bef9f01e4a0cb9f345638ee351f02a
Rob-Yoo <jin12243@naver.com>
2024-08-02 18:11:22 +0900

    BirthdayViewController 연습:

--- SeSACRxThreads/SignUpOptional/BirthdayViewController.swift
+++ SeSACRxThreads/SignUpOptional/BirthdayViewController.swift
@@ -7,6 +7,8 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class BirthdayViewController: UIViewController {
     
@@ -16,6 +18,7 @@ class BirthdayViewController: UIViewController {
         picker.preferredDatePickerStyle = .wheels
         picker.locale = Locale(identifier: "ko-KR")
         picker.maximumDate = Date()
+        picker.date = .now
         return picker
     }()
     
@@ -36,7 +39,6 @@ class BirthdayViewController: UIViewController {
     
     let yearLabel: UILabel = {
        let label = UILabel()
-        label.text = "2023년"
         label.textColor = Color.black
         label.snp.makeConstraints {
             $0.width.equalTo(100)
@@ -46,7 +48,6 @@ class BirthdayViewController: UIViewController {
     
     let monthLabel: UILabel = {
        let label = UILabel()
-        label.text = "33월"
         label.textColor = Color.black
         label.snp.makeConstraints {
             $0.width.equalTo(100)
@@ -56,7 +57,6 @@ class BirthdayViewController: UIViewController {
     
     let dayLabel: UILabel = {
        let label = UILabel()
-        label.text = "99일"
         label.textColor = Color.black
         label.snp.makeConstraints {
             $0.width.equalTo(100)
@@ -66,18 +66,69 @@ class BirthdayViewController: UIViewController {
   
     let nextButton = PointButton(title: "가입하기")
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = Color.white
         
         configureLayout()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(SearchViewController(), animated: true)
+    func bind() {
+        let current = Calendar.current.dateComponents([.year, .month, .day], from: .now)
+        let year = BehaviorRelay(value: current.year!)
+        let month = BehaviorRelay(value: current.month!)
+        let day = BehaviorRelay(value: current.day!)
+        let age = BehaviorRelay(value: 0)
+        
+        birthDayPicker.rx.date
+            .bind(with: self) { owner, date in
+                let dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: date)
+                
+                age.accept(current.year! - dateComponents.year!)
+                year.accept(dateComponents.year!)
+                month.accept(dateComponents.month!)
+                day.accept(dateComponents.day!)
+            }
+            .disposed(by: disposeBag)
+        
+        age.map { $0 >= 17 }
+            .bind(with: self) { owner, value in
+                let infoText = value ? "가입 가능한 나이입니다." : "만 17세 이상만 가입 가능합니다."
+                let infoColor: UIColor = value ? .blue : .red
+                let buttonColor: UIColor = value ? .black : .lightGray
+                
+                owner.infoLabel.text = infoText
+                owner.infoLabel.textColor = infoColor
+                owner.nextButton.backgroundColor = buttonColor
+                owner.nextButton.isEnabled = value
+            }
+            .disposed(by: disposeBag)
+        
+        year
+            .map { String($0) + "년" }
+            .bind(to: yearLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        month
+            .map { String($0) + "월" }
+            .bind(to: monthLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        day
+            .map { String($0) + "일" }
+            .bind(to: dayLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(SearchViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
     }
 
     
